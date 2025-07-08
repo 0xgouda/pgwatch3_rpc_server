5865c334985bc3af510997d77f9a226a0cba395a
0xgouda <gouda0x@gmail.com>
2025-07-08 13:17:00 +0300

    [*] update kafka receiver to use gRPC protobuf types

--- cmd/kafka_prod_receiver/main.go
+++ cmd/kafka_prod_receiver/main.go
@@ -18,7 +18,6 @@ func main() {
 		return
 	}
 
-	var server sinks.Receiver
 	server, err := NewKafkaProducer(*kafkaHost, nil, nil, *autoadd)
 	if err != nil {
 		log.Println("[ERROR]: Unable to create Kafka Producer ", err)
